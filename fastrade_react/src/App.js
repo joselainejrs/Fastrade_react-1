615e788350b25d40b5542755d44a14b454f4fae0
FIC <FIC@info.local>
2019-11-28 18:03:19 -0200

    Componentizaçao

--- fastrade_react/src/App.js
+++ fastrade_react/src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+// Inportamos os nossos componentes
+import Rodape from './components/rodape/Rodape';
+import Header from './components/cabecalho/cabecalho';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Rodape />
     </div>
   );
 }
