c0f0b7bfe8c848d0b95e1931cfbc109f5702aaa7
FIC <jennifer1996santos@hotmail.com>
2019-11-28 17:59:16 -0200

    Componentizaçao

--- fastrade_react/src/components/cabecalho/cabecalho.js
+++ fastrade_react/src/components/cabecalho/cabecalho.js
@@ -1,77 +1,37 @@
-
-import React, { Component } from 'react';
-import Logo from '../../assets/img/fastrade_img.png';
+import React from 'react';
+import Logotipo from '../../assets/img/FONTE-1.png';
+import User from '../../assets/img/user.png';
+import Lupa from '../../assets/img/search.png';
 import '../../assets/css/cabecalho.css';
-import { Link, withRouter } from 'react-router-dom';
-import { usuarioAutenticado, parseJwt } from '../../services/auth';
 
-class Cabecalho extends Component {
-    logout = () => {
-        //Remove o token do localStorage
-        localStorage.removeItem("");
-
-        //Redireciona para o endereço '/'
-        this.props.history.push('/');
-    }
-
-    render() {
-        return (
-            <div>
-                <header className="cabecalhoPrincipal">
-                    <div className="container">
-                        <img src={Logo} alt="Logo do site." />
-
-                        <nav className="cabecalhoPrincipal-nav">
-                            <Link to="/">Index</Link>
-
-                            {usuarioAutenticado() && parseJwt().Role === "Cliente_Fornecedor" ? (
-                                <>
-                                    <Link to="/produtos">Produtos</Link>
-                                    <a href="/#" onClick={this.logout}>Sair</a>
-                                </>
-
-                                <>
-                                    <Link to="/dicas">Dicas</Link>
-                                    <a href="/#" onClick={this.logout}>Sair</a>
-                                </>
-
-                                <>
-                                    <Link to="/receitas">Receitas</Link>
-                                    <a href="/#" onClick={this.logout}>Sair</a>
-                                </>
-
-                                <>
-                                    <Link to="/quem somos">Quem somos</Link>
-                                    <a href="/#" onClick={this.logout}>Sair</a>
-                                </>
-
-                                <>
-                                    <Link to="/cadastro">Cadastro</Link>
-                                    <a href="/#" onClick={this.logout}>Sair</a>
-                                </>
-                            
-                        ) : (
-                                    usuarioAutenticado() && parseJwt().Role === "" ? (
-                                        <React.Fragment>
-                                            <Link to="/"></Link>
-                                            <a href="/#" onClick={this.logout}>Sair</a>
-                                        </React.Fragment>
-                                    ) : (
-
-                                            <React.Fragment>
-                                                <Link className="cabecalhoPrincipal-nav-login" to="/login">Login</Link>
-                                            </React.Fragment>
-                                        )
-
-                                )}
-                            {/*? == if */}
-                            {/* : == else */}
-                        </nav>
+function Header() {
+    return (
+        <div>
+            <header>
+            <div className="container">
+                <div className="sub_menu">
+                    <img src={Logotipo} alt="Logo do site" className="logo"/>
+    
+                    <div className="pesquisa">
+                        <input type="text" placeholder="Busque aqui..." aria-label="Barra de busca" name="Barra_busca"
+                            className="txt_busca"/>
+                        <img src={Lupa} alt="Buscar" className="btn_busca"/>
                     </div>
-                </header>
+                   
+                    <a href="index_comprador.html"><img src={User} alt="Entra em sua conta"/></a>
+                </div>
+                <nav>
+                    <ul>
+                        <li><a href="index.html">HOME</a></li>
+                        <li><a href="produtos.html">PRODUTOS</a></li>
+                        <li><a href="Dicas.html">DICAS</a></li>
+                        <li><a href="Quem Somos .html">QUEM SOMOS</a></li>
+                    </ul>
+                </nav>
             </div>
-        )
-    }
+            </header>
+        </div>
+    );
 }
 
-export default withRouter(Cabecalho);
+export default Header;
