8313ae0f5b79b63af519cc855b2c000febc8ca71
FIC <FIC@info.local>
2019-11-29 12:16:20 -0200

    AtualizacaoJoao

--- fastrade_react/src/index.js
+++ fastrade_react/src/index.js
@@ -1,27 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 //Importamos as dependencias necessarias:
 import { Route, HashRouter as Router, Switch, Redirect } from 'react-router-dom';
 
-//Importamos a pagina de cadastroProduto
-import cadastroProduto from './pages/cadastroProduto/cadastroProduto';
-import NaoEncontrada from './pages/NaoEncontrada/NaoEncontrada';
 
-//Realizamos a criação de rotas
+//importamos as paginas
+import App from './pages/Home/App';
+import Erro from'./pages/paginaErro/Erro'
 
+//Realizamos a criação das rotas:
 const Rotas = (
-    <Router>
-        <Switch>
-            <div>
-                <Route exact path="/" component={App} />
-                <Route path="/cadastroProduto" component={cadastroProduto} />
-                <Route component ={NaoEncontrada} />
-            </div>
-        </Switch>
-    </Router>
+	<Router>
+		<div>
+		<Switch>
+			{/* Definimos a rota para as pagina */}
+			<Route exact path="/" component={App}/>
+			<Route exact path="/Home" component={App}/>
+			<Route component={Erro}/>
+			</Switch>
+		</div>
+	</Router>
 )
 
 //Trocamos ao App padrão pelas nossas rotas
