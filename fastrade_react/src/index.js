a5e47035a58111b760473d9b7ede4099030e3b6b
Jennifer <jennifer1996santos@hotmail.com>
2019-12-04 11:01:51 -0200

    fastrade-dicas

--- fastrade_react/src/index.js
+++ fastrade_react/src/index.js
@@ -1,16 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import './assets/css/estilo.css'
 import * as serviceWorker from './serviceWorker';
 //Importamos as dependencias necessarias:
-import { Route, HashRouter as Router, Switch, Redirect } from 'react-router-dom';
+import { Route, HashRouter as Router, Switch} from 'react-router-dom';
 
 
 //importamos as paginas
 import App from './pages/Home/App';
-import Erro from'./pages/paginaErro/Erro'
+import Dicas from './pages/Dicas/Dicas';
+import './assets/css/dicas.css';
+
+import Receitas from './pages/Receitas/receita';
+import './assets/css/receita.css';
+
+import Pudim from './pages/Receitas/Pudim';
+import './assets/css/receita.css';
+
+import Arroz from './pages/Receitas/Arroz';
+import './assets/css/receita.css';
+
+import Doce from './pages/Receitas/Doce';
+import './assets/css/receita.css';
+
+import Bolo from './pages/Receitas/Bolo';
+import './assets/css/receita.css';
+
+import Farofa from './pages/Receitas/Farofa';
+import './assets/css/receita.css';
+
+import Brigadeiro from './pages/Receitas/Brigadeiro';
+import './assets/css/receita.css';
+
+import Creme from './pages/Receitas/Creme';
+import './assets/css/receita.css';
+
+import Pate from './pages/Receitas/Pate';
+import './assets/css/receita.css';
+
+
+
+import Erro from'./pages/paginaErro/Erro';
+
+
 
 //Realizamos a criação das rotas:
 const Rotas = (
@@ -19,7 +52,17 @@ const Rotas = (
 		<Switch>
 			{/* Definimos a rota para as pagina */}
 			<Route exact path="/" component={App}/>
-			<Route exact path="/Home" component={App}/>
+			<Route  path="/Home" component={App}/>
+			<Route  path="/Dicas" component={Dicas}/>
+			<Route  path="/receitas" component={Receitas}/>
+			<Route path="/pudim" component={Pudim}/>
+			<Route path="/arroz" component={Arroz}/>
+			<Route path="/Doce de Casca de Maracujá" component={Doce}/>
+			<Route path="/Bolo de casca de abóbora" component={Bolo}/>
+			<Route path="/Farofa de Casca de Melão" component={Farofa}/>
+			<Route path="/Brigadeiro de Casca de Banana" component={Brigadeiro}/>
+			<Route path="/Creme de Couve-Flor" component={Creme}/>
+			<Route path="/Patê de Alho Poró" component={Pate}/>
 			<Route component={Erro}/>
 			</Switch>
 		</div>
